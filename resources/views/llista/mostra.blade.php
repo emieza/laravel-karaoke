3d9d2072344fc36f68d67c1164ac219b1d1461ef
Enric Mieza <emieza@xtec.cat>
2017-02-03 20:30:24 +0100

    mas

--- resources/views/llista/mostra.blade.php
+++ resources/views/llista/mostra.blade.php
@@ -7,23 +7,27 @@
 <p>Lloc: {{$llista->lloc}}</p>
 <p>Organitza: {{$llista->organitzador}}</p>
 
+@if( ! $admin )
 <button class="btn btn-success" onclick="location.href='{{url('/llista/')}}/{{$llista->id}}/crea'">Demana torn per cantar!</button>
+@endif
 
 <h2>Propers temes:</h2>
-<ul>
+<ul class="list-group">
 	@foreach ($cua as $tema)
-		<li>
+		<li class="list-group-item">
 			<b>{{$tema->tema}}</b> a càrrec de <b>{{$tema->cantants}}</b>
-			<button class="btn btn-warning" onclick="">Fet!</button>
+			@if( $admin )
+				<button class="btn btn-warning" onclick="">Fet!</button>
+			@endif
 		</li>
 	@endforeach
 </ul>
 
 @if( ! $admin )
 <h2>Temes fets. Vota'ls!!</h2>
-<ul>
+<ul class="list-group">
 	@foreach ($fets as $tema)
-		<li>
+		<li class="list-group-item">
 			<b>{{$tema->tema}}</b> a càrrec de {{$tema->cantants}}
 			<button class="btn btn-primary" onclick="">M'agrada</button>
 		</li>
