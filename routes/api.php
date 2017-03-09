8ab648decd6b58380545c60b135bad849a622d77
Enric Mieza <emieza@xtec.cat>
2017-03-09 17:47:10 +0100

    espais enlloc de tabs

--- routes/api.php
+++ routes/api.php
@@ -22,82 +22,82 @@ Route::get('/user', function (Request $request) {
 
 
 Route::get('/fet/{id}', function($id) {
-	try {
-		$tema = Tema::find($id);
-		if( $tema ) {
-			$tema->fet = true;
-			$tema->save();
-			return response()->json([
-					"status" => "OK",
-					"fet" => true,
-					"message" => "tema marcat com a fet"
-				]);
-		}
-		return response()->json([
-				"status" => "ERROR",
-				"message" => "tema no trobat"
-			]);
-	}
-	catch (Exception $e) {
-		return response()->json([
-					"status" => "ERROR",
-					"message" => $e->getMessage()
-				]);
-	}
+    try {
+        $tema = Tema::find($id);
+        if( $tema ) {
+            $tema->fet = true;
+            $tema->save();
+            return response()->json([
+                    "status" => "OK",
+                    "fet" => true,
+                    "message" => "tema marcat com a fet"
+                ]);
+        }
+        return response()->json([
+                "status" => "ERROR",
+                "message" => "tema no trobat"
+            ]);
+    }
+    catch (Exception $e) {
+        return response()->json([
+                    "status" => "ERROR",
+                    "message" => $e->getMessage()
+                ]);
+    }
 });
 
 Route::get('/nvots/{id}', function(Request $request, $id) {
-	try {
-		$nvots = Vot::where("tema_id",$id)->get()->count();
-		return response()->json([
-					"status" => "OK",
-					"tema_id" => $id,
-					"nvots" => $nvots,
-				]);
-	} catch (Exception $e) {
-		return response()->json([
-					"status" => "ERROR",
-					"message" => $e->getMessage()
-				]);
-	}
+    try {
+        $nvots = Vot::where("tema_id",$id)->get()->count();
+        return response()->json([
+                    "status" => "OK",
+                    "tema_id" => $id,
+                    "nvots" => $nvots,
+                ]);
+    } catch (Exception $e) {
+        return response()->json([
+                    "status" => "ERROR",
+                    "message" => $e->getMessage()
+                ]);
+    }
 });
 
 
 Route::get('/vota/{id}', function(Request $request, $id) {
-	try {
-		$ip = $request->ip();
-		$tema = Tema::find($id);
-		// si ja esta votat aquest tema des d'aquesta ip no el deixem tornar a afegir
-		$votat = Vot::where("tema_id",$id)->where("ip",$ip)->get()->count();
-		if( $votat ) {
-			$votat = Vot::where("tema_id",$id)->where("ip",$ip)->delete();
-			$nvots = Vot::where("tema_id",$id)->get()->count();
-			return response()->json([
-						"status" => "OK",
-						"vot" => false,
-						"nvots" => $nvots,
-						"message" => "Estat actual = NO votat"
-					]);
-		}
-		// creem vot i l'afegim a la BD
-		$vot = new Vot();
-		$vot->tema_id = $tema->id;
-		$vot->ip = $ip;
-		$vot->comentaris = "";
-		$vot->save();
-		$nvots = Vot::where("tema_id",$id)->get()->count();
-		return response()->json([
-					"status" => "OK",
-					"vot" => true,
-					"nvots" => $nvots,
-					"message" => "Estat actual = votat"
-				]);
-	} catch (Exception $e) {
-		return response()->json([
-					"status" => "ERROR",
-					"message" => $e->getMessage()
-				]);
-	}
+    try {
+        $ip = $request->ip();
+        $tema = Tema::find($id);
+        // si ja esta votat aquest tema des d'aquesta ip no el deixem tornar a afegir
+        $votat = Vot::where("tema_id",$id)->where("ip",$ip)->get()->count();
+        if( $votat ) {
+            $votat = Vot::where("tema_id",$id)->where("ip",$ip)->delete();
+            $nvots = Vot::where("tema_id",$id)->get()->count();
+            return response()->json([
+                        "status" => "OK",
+                        "vot" => false,
+                        "nvots" => $nvots,
+                        "message" => "Estat actual = NO votat"
+                    ]);
+        }
+        // creem vot i l'afegim a la BD
+        $vot = new Vot();
+        $vot->tema_id = $tema->id;
+        $vot->ip = $ip;
+        $vot->comentaris = "";
+        $vot->save();
+        $nvots = Vot::where("tema_id",$id)->get()->count();
+        return response()->json([
+                    "status" => "OK",
+                    "vot" => true,
+                    "nvots" => $nvots,
+                    "message" => "Estat actual = votat"
+                ]);
+    } catch (Exception $e) {
+        return response()->json([
+                    "status" => "ERROR",
+                    "message" => $e->getMessage()
+                ]);
+    }
 });
 
 
